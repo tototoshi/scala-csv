b7e97eebb6052305e3cebccbe79dbbd9b775bb20
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-02-09 15:24:49 +0900

    Updated sbt to version 0.12.2. Cross building for 2.9.1, 2.9.2 and 2.10.0.

---
+++ project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+
+object ScalaCSVProject extends Build {
+
+  lazy val root = Project (
+    id = "scala-csv",
+    base = file ("."),
+    settings = Defaults.defaultSettings ++ Seq (
+      name := "scala-csv",
+      version := "0.3",
+      crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.0"),
+      organization := "com.github.tototoshi",
+      libraryDependencies ++= Seq(
+        "net.sf.opencsv" % "opencsv" % "2.3",
+        "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+      )
+    )
+  )
+}
+
--- sbt
+++ sbt
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dsbt.log.noformat=true -Xdebug -Xmx2048M -XX:+CMSClassUnloadingEnabled -XX:PermSize=256M -jar ./sbt-launch-0.11.jar "$@"
+java -Dsbt.log.noformat=true -Xdebug -Xmx2048M -XX:+CMSClassUnloadingEnabled -XX:PermSize=256M -jar ./sbt-launch-0.12.2.jar "$@"
