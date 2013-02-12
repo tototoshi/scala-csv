c8d51d06a78a8a5551388630fa4ceb43a66a9a29
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-02-12 21:47:27 +0900

    Added settings for publishing.

--- project/Build.scala
+++ project/Build.scala
@@ -14,8 +14,40 @@ object ScalaCSVProject extends Build {
       libraryDependencies ++= Seq(
         "net.sf.opencsv" % "opencsv" % "2.3",
         "org.scalatest" %% "scalatest" % "1.9.1" % "test"
-      )
+      ),
+      publishMavenStyle := true,
+      publishTo <<= version { (v: String) =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+      publishArtifact in Test := false,
+      pomExtra := _pomExtra
     )
   )
+
+  val _pomExtra =
+    <url>http://github.com/tototoshi/scala-csv</url>
+    <licenses>
+      <license>
+        <name>Apache License, Version 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:tototoshi/scala-csv.git</url>
+      <connection>scm:git:git@github.com:tototoshi/scala-csv.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>tototoshi</id>
+        <name>Toshiyuki Takahashi</name>
+        <url>http://tototoshi.github.com</url>
+      </developer>
+    </developers>
+
 }
 
