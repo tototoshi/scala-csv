b68a4d02a387a2219d68a5c09e190023bad5676a
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-03-05 21:08:07 +0900

    Release for Scala 2.9.3

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ScalaCSVProject extends Build {
     settings = Defaults.defaultSettings ++ Seq (
       name := "scala-csv",
       version := "0.7.0",
-      crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.0"),
+      crossScalaVersions := Seq("2.9.1", "2.9.2", "2.9.3", "2.10.0"),
       organization := "com.github.tototoshi",
       libraryDependencies ++= Seq(
         "net.sf.opencsv" % "opencsv" % "2.3",
