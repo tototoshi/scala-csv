f02b6eab58cb815510c83717becb92341b98c2ad
toshiyuki <t.toshi.0412@gmail.com>
2013-05-06 13:13:35 +0900

    Bumped version

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ScalaCSVProject extends Build {
     base = file ("."),
     settings = Defaults.defaultSettings ++ Seq (
       name := "scala-csv",
-      version := "0.8.0",
+      version := "1.0.0-SNAPSHOT",
       crossScalaVersions := Seq("2.9.1", "2.9.2", "2.9.3", "2.10.0"),
       organization := "com.github.tototoshi",
       libraryDependencies ++= Seq(
