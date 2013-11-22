95dc9f8e031c3cefbbb271f446af942019b455c0
toshiyuki <t.toshi.0412@gmail.com>
2013-11-22 23:39:37 +0900

    Added license header

--- src/main/scala/com/github/tototoshi/csv/Quoting.scala
+++ src/main/scala/com/github/tototoshi/csv/Quoting.scala
@@ -1,12 +1,20 @@
+/*
+ * Copyright 2013 Toshiyuki Takahashi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.tototoshi.csv
 
-/**
- * Created with IntelliJ IDEA.
- * User: toshi
- * Date: 2013/05/06
- * Time: 14:16
- * To change this template use File | Settings | File Templates.
- */
 sealed abstract trait Quoting
 case object QUOTE_ALL extends Quoting
 case object QUOTE_MINIMAL extends Quoting
