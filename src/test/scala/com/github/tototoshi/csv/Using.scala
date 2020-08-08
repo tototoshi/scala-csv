3b86f5eb16c538d715ca30424f8652d26e3e8da7
xuwei-k <6b656e6a69@gmail.com>
2020-08-09 00:46:28 +0900

    avoid structural type

--- src/test/scala/com/github/tototoshi/csv/Using.scala
+++ src/test/scala/com/github/tototoshi/csv/Using.scala
@@ -16,10 +16,30 @@
 
 package com.github.tototoshi.csv
 
+import java.io.Closeable
+import scala.io.BufferedSource
+
+trait CanClose[A] {
+  def close(a: A): Unit
+}
+
+object CanClose {
+  def instance[A](f: A => Unit): CanClose[A] =
+    new CanClose[A] {
+      override def close(a: A) = f(a)
+    }
+
+  implicit def closeable[A <: Closeable]: CanClose[A] =
+    instance(_.close())
+
+  implicit val source: CanClose[BufferedSource] =
+    instance(_.close())
+}
+
 protected trait Using {
 
-  def using[A, R <: { def close(): Unit }](r: R)(f: R => A): A =
-    try { f(r) } finally { r.close() }
+  def using[A, R](r: R)(f: R => A)(implicit c: CanClose[R]): A =
+    try { f(r) } finally { c.close(r) }
 
 }
 
