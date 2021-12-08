914bdac2aaeed2571c6377fac4392755ae16b7cd
Jason Fine <jasonf20@gmail.com>
2021-12-08 12:40:17 +0200

    SourceLineReader: Fixed issue with reading \r chars that are not followed by \n
    This normalizes this behaviour of SourceLineReader to behave the same as ReaderLineReader for these values.
    Additionally the previous handling would cause parse errors if certain situations

--- src/test/scala/com/github/tototoshi/csv/LineReaderSpec.scala
+++ src/test/scala/com/github/tototoshi/csv/LineReaderSpec.scala
@@ -35,5 +35,15 @@ class LineReaderSpec extends AnyFunSpec with Matchers with Using {
       }
     }
 
+    it("should read a quoted value that has cr that is not followd by nl") {
+      using(Source.fromFile("src/test/resources/has-cr-quoted-value.csv")) { in =>
+        using(new SourceLineReader(in)) { reader =>
+          reader.readLineWithTerminator() should be("a,\"b\r")
+          reader.readLineWithTerminator() should be("\",c\n")
+          reader.readLineWithTerminator() should be("d,e,f\n")
+        }
+      }
+    }
+
   }
 }
